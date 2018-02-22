65deea057a2e3c01b96c5870cc63cf7d96d2f4e2
Carlos <carlos.w.montecinos@gmail.com>
2018-02-22 00:17:04 -0300

    Added Template for setup.py

--- do/setup_config.py
+++ do/setup_config.py
@@ -1,4 +1,8 @@
 from string import Template
+import os
+
+path_setup = os.path.join(
+    os.path.dirname(__file__), 'templates\\template_setup.txt')
 
 
 def setup_template(
@@ -9,40 +13,14 @@ def setup_template(
     setup_author_email,
     setup_url,
 ):
-    """
-    Template for the setup.py and the functions that allows
-    you to replace the basics fields.
-    """
-    # TODO: move the setup template to file
-    setup_file = Template(
-        """# -*- coding: utf-8 -*-
+    with open(path_setup, 'r') as f:
+        setup_content = Template(f.read())
 
-from setuptools import setup, find_packages
-
-
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
-setup(
-    name='$setup_name',
-    version='$setup_version',
-    description='$setup_description',
-    long_description=readme,
-    author='$setup_author',
-    author_email='$setup_author_email',
-    url='$setup_url',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
-)"""
-    )
-    return setup_file.substitute(
+    return setup_content.substitute(
         setup_name=setup_name,
         setup_version=setup_version,
         setup_description=setup_description,
         setup_author=setup_author,
         setup_author_email=setup_author_email,
-        setup_url=setup_url,
+        setup_url=setup_url
     )
