319d11eeb181d1ccc4c85dc20054e8d040b378ba
Carlos <carlos.w.montecinos@gmail.com>
2018-02-22 23:49:20 -0300

    Code Refatoring

--- do/licenses.py
+++ do/licenses.py
@@ -1,11 +1,6 @@
-from datetime import datetime
-from string import Template
-import json
-import os
-import click
-import sys
-
 """
+Uses to template the licenses:
+
 $project - $year - $fullname
 -------------------------------------
 GNU_GPLv3   = year, fullname, project
@@ -18,6 +13,13 @@ Mozilla     = None
 Unlicensed  = None
 """
 
+from datetime import datetime
+from string import Template
+import json
+import os
+import click
+import sys
+
 # short names for licenses
 apache2 = 'Apache License 2.0'
 gnuAgpl = 'GNU Affero General Public License v3'
@@ -25,25 +27,42 @@ gnuGpl = 'GNU General Public License v3'
 bsd = 'BSD License'
 mit = 'MIT License'
 
-# relativa path tho the licenses template folder
+# relative path tho the licenses template folder
 template_path = os.path.join(
     os.path.dirname(__file__), 'templates\\licenses\\')
 
 # current year
 year = str(datetime.now().year)
 
-# open the index.json that contains the license names and filenames
-try:
-    # ! error in travis!!!!
-    with open(os.path.join(template_path, 'index.json'), 'r') as i:
-        license_list = json.load(i)
-except FileNotFoundError:
-    click.echo('LicenseNotFoundError: {} Not Found. Aborted!'
-               .format(template_path + 'index.json'))
-    sys.exit(1)
+
+def load_index_json(path=template_path):
+    # open the index.json that contains the license names and filenames
+    try:
+        # ! error in travis!!!!
+        with open(os.path.join(path, 'index.json'), 'r') as i:
+            return json.load(i)
+    except FileNotFoundError:
+        click.echo('LicenseNotFoundError: {} Not Found. Aborted!'
+                   .format(template_path + 'index.json'))
+        sys.exit(1)
 
 
-def show(index_json=license_list):
+def load_license_content(license_name):
+    """
+    Load the content of a license
+    """
+    index_json = load_index_json()
+    # open the license templates and use Template() to replace variables
+    try:
+        with open(template_path + index_json[license_name], 'r') as f:
+            return Template(f.read())
+    except FileNotFoundError:
+        click.echo('LicenseNotFoundError: {} Not Found. Aborted!'
+                   .format(template_path + index_json[license_name]))
+        sys.exit(1)
+
+
+def show(index_json=load_index_json()):
     """
     Prints a the list of licenses in the index.json for the user to choose
     """
@@ -57,32 +76,28 @@ def show(index_json=license_list):
 def choose(license_name, author_name=None, project=None):
     """
     Allows to Choose one license, but only in assistant mode.
+
     """
-    # open the license templates and use Template() to replace variables
-    try:
-        with open(template_path + license_list[license_name], 'r') as f:
-            license_content = Template(f.read())
-    except FileNotFoundError:
-        click.echo('LicenseNotFoundError: {} Not Found. Aborted!'
-                   .format(template_path + license_list[license_name]))
-        sys.exit(1)
+    # the call to the function that load the content a license
+    lic_name = load_license_content(license_name)
+
     # licenses that need year and author name
     if (license_name == (apache2) or (license_name == bsd) or
         (license_name == gnuAgpl) or
             (license_name == mit)):
-        return license_content.substitute(
+        return lic_name.substitute(
             year=year,
             fullname=author_name
         )
     # licenses that need year, author name and project name
     elif license_name == gnuGpl:
-        return license_content.substitute(
+        return lic_name.substitute(
             year=year,
             fullname=author_name,
             project=project
         )
     else:
-        return license_content.substitute()
+        return lic_name.substitute()
 
 
 if __name__ == '__main__':
